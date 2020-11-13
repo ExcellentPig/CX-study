97b4108b0e65a9cb59c2438970e263e5ee19cf59
ExcellentPig <894399743@qq.com>
2020-11-13 17:57:52 +0800

    2020-11-13

---
+++ study/lkxy-DesignPatterns/Part2/js/MyTool.js
@@ -0,0 +1,35 @@
+// function myToll () {
+// 	// 私有数据
+// 	var money = 1000
+// 	// 提供操作私有数据的函数
+// 	function get() {
+// 		money *= 10
+// 		console.log(`加了${money}元`)
+// 	}
+// 	function send() {
+// 		money --;
+// 		console.log(`减了${money}元`)
+// 	}
+// 	return {
+// 		'get': get,
+// 		'send': send
+// 	}
+// }
+
+(function(w) {
+	var money = 1000
+	// 提供操作私有数据的函数
+	function get() {
+		money *= 10
+		console.log(`加了${money}元`)
+	}
+
+	function send() {
+		money--;
+		console.log(`减了${money}元`)
+	}
+	w.myTool = {
+		'get': get,
+		'send': send
+	}
+})(window)
